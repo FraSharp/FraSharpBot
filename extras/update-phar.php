5687973a56078381c614926cf29371ebe4a2dde2
skrtdev <64538010+skrtdev@users.noreply.github.com>
2020-12-03 20:37:02 +0100

    ok

--- extras/update-phar.php
+++ extras/update-phar.php
@@ -1,20 +1,17 @@
 <?php
-/* 
+/*
 
     this is an automated method for updating the Bot
     in case you're using phar.
 
     Requirements:
-    -- Linux VM (for deleting the old "novagram.phar" file)
-    -- Hosting allows to do the "shell_exec()" command
     -- using phar
     -- obviously not using composer :|
 
 */
 
-if ($text === "/updatePhar") 
-{
+$Bot->onCommand("updatePhar", function ($message) {
     $message->delete(null, true);  // "null, true" means that you're using json_payload for deleting the message
     unlink("novagram.phar");  // change "novagram.phar" with the name of your .phar file (if you have a different name)
-    $chat->sendMessage("I've updated the bot to the latest version");
-}
+    $message->reply("I've updated the bot to the latest version");
+});
