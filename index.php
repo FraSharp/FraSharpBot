2ff04393db3a9dbb49fe6fbbb4084edb0156461c
skrtdev <64538010+skrtdev@users.noreply.github.com>
2020-12-03 20:31:28 +0100

    boh

--- index.php
+++ index.php
@@ -1,32 +1,26 @@
 <?php
-use skrtdev\Telegram\Message;
-
-header('Content-Type: application/json');
-
 if (file_exists('vendor')) {
     require 'vendor/autoload.php';
 }
-
-//phar
-
-else{
+else{ //phar
     if (!file_exists('novagram.phar')) {
         copy('http://gaetano.cf/novagram/phar.php', 'novagram.phar');
     }
     require_once 'novagram.phar';
 }
 
-$Bot = new \skrtdev\NovaGram\Bot("YOUR_BOT_TOKEN", [
-    "json_payload" => true,
+use skrtdev\Telegram\Message;
+use skrtdev\NovaGram\Bot;
+
+$Bot = new Bot("YOUR_BOT_TOKEN", [
     "debug" => YOUR_USER_ID,    // your user_id
     "parse_mode" => 'HTML',     // you can use "Markdown" for Markdown
 ]);
 
-
 $update = $Bot->update;                                                     // update
 $message = $update->message;                                                // message
 $text = $message->text;                                                     // text the message
-$chat = $message->chat;                                                     // chat                         
+$chat = $message->chat;                                                     // chat
 $user = $message->from;                                                     // user
 $useridReply = $message->reply_to_message->from->id;                        // user_id of user in reply
 $usernameReply = $message->reply_to_message->from->username;                // username of user in reply
@@ -59,7 +53,7 @@ try {
 
         // command to change chat's title (if is a group/supergroup)
         // example of usage: "/changeTitle i choose this title" the title of the chat will be "i choose this title"
-        if (strpos($text, "/changeTitle") === 0) 
+        if (strpos($text, "/changeTitle") === 0)
         {
             $ai = explode(" ", $text);
             unset($ai[0]);
@@ -79,7 +73,7 @@ try {
 
 
         // command to unpin the pinned message
-        if ($text === "/unpinMessage") 
+        if ($text === "/unpinMessage")
         {
             $unpinChatMessage = $chat->unpinMessage();
             // checks if there is a pinned message in the chat
@@ -88,7 +82,7 @@ try {
 
 
         // command to unpin all the pinned messages in a group/supergroup
-        if ($text === "/unpinAllMessages") 
+        if ($text === "/unpinAllMessages")
         {
             if ($chat->pinned_message == false) $chat->sendMessage("no pinned messages");
             else $chat->unpinAllMessages();
@@ -96,7 +90,7 @@ try {
 
 
         // command to mute the user in reply
-        if ($text == "/muteUser") 
+        if ($text == "/muteUser")
         {
             $restrict = $Bot->restrictChatMember
             ([
@@ -109,7 +103,7 @@ try {
 
 
         // command to unmute the user in reply
-        if ($text == "/unmuteUser") 
+        if ($text == "/unmuteUser")
         {
             $Bot->restrictChatMember
             ([
@@ -121,7 +115,7 @@ try {
 
 
         // command to make admin a user
-        // example: "/promoteMember 1 1 1 1 1" 
+        // example: "/promoteMember 1 1 1 1 1"
         if ((strpos($text, "/promoteMember") === 0) and ($replyToMessage !== null and $usernameReply !== null))
         {
             $ai = str_replace("admin ", "", $text);
@@ -142,7 +136,7 @@ try {
 
 
         // command to demote an admin to user
-        if ((strpos($text, "/demoteMember") === 0) and ($replyToMessage !== null and $usernameReply !== null)) 
+        if ((strpos($text, "/demoteMember") === 0) and ($replyToMessage !== null and $usernameReply !== null))
         {
             $Bot->promoteChatMember
             ([
@@ -163,10 +157,10 @@ try {
             $chatAdmins = $chat->getAdministrators($chatid);
 
             $adminsar = [];
-            foreach ($chatAdmins as $chatAdmin) 
+            foreach ($chatAdmins as $chatAdmin)
             {
                 if ($chatAdmin === '[ChatMember]') continue;
-                
+
                 $adminsar[] = "<a href='t.me/{$chatAdmin->user->username}'>@" . ($chatAdmin->user->username) . "</a>\n";
             }
 
@@ -175,14 +169,14 @@ try {
 
 
         // command to check if a user is an admin
-        if ($text == "/isAdmin?") 
+        if ($text == "/isAdmin?")
         {
             $chatMember = $chat->getMember($useridReply);
 
             $isAdmin = in_array($chatMember->status, ["administrator", "creator"]);
 
             if ($isAdmin) $chat->sendMessage("@" . $usernameReply . " is an admin");
-            else 
+            else
             {
                 if (!in_array($chatMember->status, ["administrator", "creator"])) $chat->sendMessage("@" . $usernameReply . " isn't an admin");
             }
@@ -190,7 +184,7 @@ try {
 
 
         // check lag (?)
-        if ($text === "/checkLag") 
+        if ($text === "/checkLag")
         {
             $start = hrtime(true);
             $lagMex = $chat->sendMessage("lag...");
@@ -200,7 +194,7 @@ try {
 
 
         // command to get user_id of user in reply
-        if ($text === "/userId") 
+        if ($text === "/userId")
         {
             $chat->sendMessage("@$usernameReply id: <code>$useridReply</code>");
         }
@@ -209,8 +203,8 @@ try {
         // command to send commands to your VM
         // use this if your VM is based on Linux
         // use if hosting provider allows to do the "shell_exec()" command
-        // usage: /command command, example: "/command ls" 
-        if (stripos($text, "/command") === 0) 
+        // usage: /command command, example: "/command ls"
+        if (stripos($text, "/command") === 0)
         {
             $explode = explode(" ", $text);
             unset($explode[0]);
@@ -222,7 +216,7 @@ try {
     }
 
 
-} catch (Throwable $e) 
+} catch (Throwable $e)
 {
     file_put_contents("logs.txt", $e . "\n", FILE_APPEND);
     $chat->sendMessage("<b>error code: " . $e->getCode() . "</b>\n\n<b>description: </b>$e");
